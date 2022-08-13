dcb6dc05c487be9262b394d46f8bbc01d7d6d311
Sobhan Kumar Rath <59742478+sovii003@users.noreply.github.com>
2022-08-13 09:34:03 +0530

    v1

---
+++ server/index.js
@@ -0,0 +1,31 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+import postRoutes from './routes/posts.js';
+
+const app = express();
+dotenv.config();
+
+
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the API');
+})
+
+// Connect to MongoDB
+const PORT =process.env.PORT || 5000;
+
+mongoose.connect(process.env.DB_CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => { app.listen(PORT, () => console.log(`Server running at port ${PORT}`)); })
+    .catch((error) => { console.log(error.message)});
+
+// mongoose.set('useFindAndModify', false);
+
